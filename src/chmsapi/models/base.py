620d308d9fbbf62b87a7bcafc1f79a8ec9dc8b90
Festus Kwafo Ofori <68119922+Festus-Kwafo@users.noreply.github.com>
2024-02-09 08:56:15 +0000

    members model

--- src/chmsapi/models/base.py
+++ src/chmsapi/models/base.py
@@ -1,7 +1,14 @@
 from datetime import datetime
-
+from typing_extensions import Annotated
 from sqlalchemy.orm import MappedAsDataclass, Mapped, mapped_column
+from sqlalchemy import String
+from uuid import uuid4
 
 
 class DateTimeMixin(MappedAsDataclass):
     created_date: Mapped[datetime] = mapped_column(init=False, )
+
+
+id_key = Annotated[
+    int, mapped_column(String(50), primary_key=True, default_factory=uuid4, unique=True)
+]
--- src/chmsapi/models/members.py
+++ src/chmsapi/models/members.py
@@ -1,5 +1,14 @@
 from src.chmsapi.config.db import Base
-
+from sqlalchemy import Column, String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from src.chmsapi.models.base import id_key
+from datetime import datetime
 
 class Members(Base):
     __tablename__ = "members"
+    id: Mapped[id_key] = mapped_column(init=False)
+    first_name: Mapped[str] = mapped_column(String(50), default=None, comment="First Name")
+    last_name: Mapped[str] = mapped_column(String(50), default=None, comment="Last Name")
+    email: Mapped[str] = mapped_column(String(50), unique=True, index=True, comment='email address')
+    dob = Mapped[datetime] = mapped_column()
+    phone: Mapped[str | None] = mapped_column(String(11), default=None, comment='Phone number')
