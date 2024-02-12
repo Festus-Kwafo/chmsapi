e373f91806517427d5731a2ecc1a87925a955f3b
Festus Kwafo Ofori <68119922+Festus-Kwafo@users.noreply.github.com>
2024-02-12 17:07:39 +0000

    alembic migration folder added

--- src/chmsapi/config/path_conf.py
+++ src/chmsapi/config/path_conf.py
@@ -2,7 +2,7 @@ import os
 
 from pathlib import Path
 
-BasePath = Path(__file__).resolve().parent.parent.parent
+BasePath = Path(__file__).resolve().parent.parent.parent.parent
 
 # Log file path
-LogPath = os.path.join(BasePath, 'src', 'logs')
+LogPath = os.path.join(BasePath, 'src', 'chmsapi', 'logs')
